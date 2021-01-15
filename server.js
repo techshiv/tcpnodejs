d915f66e5a2237c4286d38d5ceffcdf96e63cd62
shivashankar <45993063+techshiv@users.noreply.github.com>
2021-01-15 17:32:05 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+var os=require('os');
+var net=require('net');
+
+var networkInterfaces=os.networkInterfaces();
+
+var port = 8081;
+var count = 1;
+
+function callback_server_connection(socket){
+    var remoteAddress = socket.remoteAddress;
+    var remotePort = socket.remotePort;
+    socket.setNoDelay(true);
+    console.log("connected: ", remoteAddress, " : ", remotePort);
+    
+    var msg = 'Hello ' + remoteAddress + ' : ' +  remotePort + '\r\n'
+        + "You are #" + count + '\r\n';
+    count++;
+
+    
+    
+    socket.on('data', function (data) {
+        console.log(data.toString());
+    });
+    
+    socket.on('end', function () {
+        console.log("ended: ", remoteAddress, " : ", remotePort);
+    });
+}
+
+
+
+console.log("node.js net server is waiting:");
+for (var interface in networkInterfaces) {
+
+    networkInterfaces[interface].forEach(function(details){
+        
+        if ((details.family=='IPv4') && !details.internal) {
+            console.log(interface, details.address);  
+        }
+    });
+}
+
+console.log("port: ", port);
+
+var netServer = net.createServer(callback_server_connection);
+netServer.listen(port);
